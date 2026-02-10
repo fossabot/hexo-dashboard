0f4c1adf7656831e2a86b43fea755cd94e2862ad
Meteor2333 <Meteor23333@outlook.com>
2026-02-10 23:07:13 +0800

    将element-plus依赖改为按需导入以减小构建产物体积

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,34 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import Vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    Vue(),
+    Icons({
+      autoInstall: true,
+    }),
+    AutoImport({
+      dts: false,
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({ prefix: 'Icon' }),
+      ],
+    }),
+    Components({
+      dts: false,
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({ enabledCollections: ['ep'] }),
+      ],
+    }),
   ],
   resolve: {
     alias: {
