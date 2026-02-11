a4c148bec998c07b89dc73b4d9f0cc4098feff59
Meteor2333 <Meteor23333@outlook.com>
2026-02-11 17:34:01 +0800

    修复按需引用el-plus后引起的bug

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import 'element-plus/theme-chalk/el-message.css';
+import 'element-plus/theme-chalk/el-message-box.css';
+import 'element-plus/theme-chalk/el-notification.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
 
 import './styles/global.css';
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
-import Icons from 'unplugin-icons/vite';
-import IconsResolver from 'unplugin-icons/resolver';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
@@ -12,21 +10,16 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
   plugins: [
     Vue(),
-    Icons({
-      autoInstall: true,
-    }),
     AutoImport({
       dts: false,
       resolvers: [
         ElementPlusResolver(),
-        IconsResolver({ prefix: 'Icon' }),
       ],
     }),
     Components({
       dts: false,
       resolvers: [
         ElementPlusResolver(),
-        IconsResolver({ enabledCollections: ['ep'] }),
       ],
     }),
   ],
