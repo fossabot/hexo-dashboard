e03a30261b9f8e61daa8aff868b4c48f3c9e1d75
Meteor2333 <Meteor23333@outlook.com>
2026-02-11 16:12:06 +0800

    修复登录时用户名大小写敏感的问题

--- server/src/services/AuthService.ts
+++ server/src/services/AuthService.ts
@@ -3,13 +3,19 @@ import type Hexo from 'hexo';
 import { compareSync } from 'bcryptjs';
 
 export default class AuthService {
+  private readonly users: Record<string, string> = {};
+
   constructor(
     private hexo: Hexo,
   ) {
+    const config = this.hexo.config.dashboard;
+    for (const name in config) {
+      this.users[name.toLowerCase()] = config[name];
+    }
   }
 
   authenticate(username: string, password: string): boolean {
-    const target = this.hexo.config.dashboard[username];
+    const target = this.users[username.toLowerCase()];
     if (!target) return false;
     return compareSync(password, target);
   }
